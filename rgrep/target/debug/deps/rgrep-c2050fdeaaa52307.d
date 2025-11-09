7b883f1feb4c58e390dc718cf403adbde187350f
Alex_Duna <daniel.alex17@yahoo.com>
2025-11-09 23:26:05 +0200

    Started the rgrep project.

---
+++ rgrep/target/debug/deps/rgrep-c2050fdeaaa52307.d
@@ -0,0 +1,5 @@
+/home/alexduna7/Command-Line-Rust-Program/rgrep/target/debug/deps/rgrep-c2050fdeaaa52307.d: src/main.rs
+
+/home/alexduna7/Command-Line-Rust-Program/rgrep/target/debug/deps/librgrep-c2050fdeaaa52307.rmeta: src/main.rs
+
+src/main.rs:
---
+++ rgrep/target/debug/rgrep.d
@@ -0,0 +1,1 @@
+/home/alexduna7/Command-Line-Rust-Program/rgrep/target/debug/rgrep: /home/alexduna7/Command-Line-Rust-Program/rgrep/src/main.rs
