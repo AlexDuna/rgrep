7cc874c28ebead01ce3b19eeef149be286d2c345
Alex Duna <daniel.alex17@yahoo.com>
2025-11-20 14:41:36 +0200

    Changed the structure and added new function.

---
+++ rgrep/src/lib.rs
@@ -0,0 +1,44 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments provided");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "func";
+        let contents = "\
+Test:
+test, this, function.
+Did it fail?";
+
+        assert_eq!(vec!["test, this, function."], search(query, contents));
+    }
+}
